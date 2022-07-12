fa57bc36eebd74c02b67402ddbe05431b0ac7b34
Eunseo Kim <kim.eunseo@kakao.com>
2022-07-13 00:04:55 +0900

    개발환경 설정
    - webpack
    - babel
    - eslint

---
+++ src/index.js
@@ -0,0 +1,2 @@
+const App = document.querySelector('.App');
+App.innerHTML = 'Hello World!';
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+    ],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    historyApiFallback: true,
+    hot: true,
+  },
+};
